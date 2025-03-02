d98199fa27ce03ed573d112c94fd746d280c56b1
Nikhil Badyal <nikhill773384@gmail.com>
2025-03-02 10:46:59 +0530

    My Fixes

--- app/models/security/price/provided.rb
+++ app/models/security/price/provided.rb
@@ -22,6 +22,7 @@ module Security::Price::Provided
         if response.success? && response.prices.size > 0
           price = Security::Price.new \
             security: security,
+            ticker: security.ticker,
             date: response.prices.first[:date],
             price: response.prices.first[:price],
             currency: response.prices.first[:currency]
@@ -48,6 +49,7 @@ module Security::Price::Provided
           response.prices.map do |price|
             new_price = Security::Price.find_or_initialize_by(
               security: security,
+              ticker: security.ticker,
               date: price[:date]
             ) do |p|
               p.price = price[:price]
