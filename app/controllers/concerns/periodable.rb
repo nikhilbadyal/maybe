14b01dff02a39dffe33e1189c73de113fa906e8d
Nikhil Badyal <nikhill773384@gmail.com>
2025-03-04 00:24:54 +0530

    Preference to set default_period

---
+++ app/controllers/concerns/periodable.rb
@@ -0,0 +1,15 @@
+module Periodable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_period
+  end
+
+  private
+    # @return [void]
+    def set_period
+      @period = Period.from_key(params[:period] || Current.user&.default_period)
+    rescue Period::InvalidKeyError
+      @period = Period.last_30_days
+    end
+end
